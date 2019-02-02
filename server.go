d19453ee7892b33c02cb42540255a4dc9b6d432b
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-01 22:32:14 -0800

    Creating a simple web server incrementing in complexity

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func greeting(context echo.Context) error {
+	// Context holds relevant information about the request such as data response body ...
+	return context.String(http.StatusOK, "Thanks for visiting the server!")
+}
+
+func main() {
+	fmt.Println("Server is running on port 4000!")
+
+	// Instantiate the client that is going to be trigerring the requests
+	client := echo.New() // Creating an instance of our server
+
+	client.GET("/greeting", greeting) // The framework provides us the context?
+
+}
