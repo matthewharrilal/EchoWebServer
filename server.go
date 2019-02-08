24339b5db70fc3d467f0cc8d4f852475d26566be
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-08 12:04:34 -0800

    Instantiating the network request function with a channel as a parameter

--- server.go
+++ server.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 type Region struct {
@@ -45,59 +45,66 @@ type Language struct {
 	URL  string `json:"url"`
 }
 
+func ConfigureDatabase() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
 
-// func lifeInfo(context echo.Context) error {
-// 	lifeInfo := LifeInfo{} // So you can pass this object by reference as opposed to value
-
-// 	err := json.NewDecoder(context.Request().Body).Decode(&lifeInfo) // The body of data that the user is trying to send through the request
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("Unmarshaled data >>>> %s", lifeInfo)
-// 	return context.String(http.StatusOK, "Accepted Request")
+	defer db.Close() // Close database connection after surrounding function executes
 }
 
-// func dogs(context echo.Context) error {
-// 	dog := Dog{}
+func ObtainRegion(channel chan int) Region {
+	// Pass the region that is being returned through the channel that the main go routine is going to receive
 
-// 	// Now that we have our dog object instantiated
-// 	// We have to intercept the request body and we have to unmarshal it into our dog object
-// 	err := json.NewDecoder(context.Request().Body).Decode(&dog) // Unmarshal data and write it to our dog object in memory
-// 	// Why does this function return an errror ... design decision?
+	url := "http://pokeapi.co/api/v2/pokedex/kanto/"
 
-// 	fmt.Printf("JSON from decoded data %s and our dog object %s", err, dog)
-// 	return context.String(http.StatusOK, "Accepted Request")
+	// Allows you to add added configurations to the http request and execute you can construct the request and execute with the client which add specified configurations
+	client := http.Client{
+		// If for some reason the request to the pokedex lags it will take awhile timeout allowed for request ... no more than a minute
+		Timeout: time.Second * 60,
+	}
+
+	// To construct a new request HTTP Method, desired url, and nil?
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+
+	if err != nil {
+		// Checking if error present
+		log.Fatal(err)
+	}
+
+	// Execute request and assign callback status
+	res, getErr := client.Do(req)
+
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	region := Region{}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&region)
+
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	fmt.Printf("REGION >>> ", region)
+	return region
 }
-
-// func cats(context echo.Context) error {
-// 	// This is another way of quite literally binding the json to our instantiated object
-// 	cat := Cat{}
-
-// 	err := context.Bind(&cat) // Unmarshal data and bind it immediately by writing it to our cat object
-// 	if err != nil {
-// 		fmt.Printf("Error when binding data to our cat object %s", err)
-// 	}
-
-// 	fmt.Printf("This is the cat object %s", cat)
-// 	return context.String(http.StatusOK, "Accepted Request")
-// }
 
 func main() {
 	fmt.Println("Server is running on port 4000!")
 
 	// Instantiate the client that is going to be trigerring the requests
 	client := echo.New() // Creating an instance of our server
-	// client.Use(middleware.Logger())
-	// client.GET("/greeting", greeting) // The framework provides us the context?
-	// client.POST("/lifeInfo", lifeInfo)
+	// client.GET("/region", ObtainRegion())
+	go ObtainRegion()
 
-	// // You can attach middlewares to a route
-
-	// client.POST("/dogs", dogs)
-	// client.POST("/cats", cats)
-	// client.GET("/goodbye", goodbye)
-
+	// Now that you have the region
 	client.Start(":4000")
 
 }
